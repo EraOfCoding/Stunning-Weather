ed50ded18215ed9ef0e102ea912051d2c1bab7ac
EraOfCoding <63988030+EraOfCoding@users.noreply.github.com>
2021-04-14 14:20:33 +0600

    now?

--- src/App.js
+++ src/App.js
@@ -3,7 +3,13 @@ import Main from './components/Main';
 import './style/Search.css'
 import { useState } from 'react';
 import Error from './components/Error';
-import api from '../config'
+
+const api = {
+  key: "eb0cc5039a77d1ad9b48255529de2da8",
+  base: "https://api.openweathermap.org/data/2.5/",
+}
+
+export default api
 
 function App() {
   const [query, setQuery] = useState('')
