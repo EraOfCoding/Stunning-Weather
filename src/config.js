1723a395a46ebdaf81db90469be885d8683417d8
EraOfCoding <63988030+EraOfCoding@users.noreply.github.com>
2021-04-15 10:43:20 +0600

    google search

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const api = {
-    key: "eb0cc5039a77d1ad9b48255529de2da8", //process.REACT_APP_KEY,
-    base: "https://api.openweathermap.org/data/2.5/", //process.REACT_APP_BASE,
+    key: "eb0cc5039a77d1ad9b48255529de2da8",
+    base: "https://api.openweathermap.org/data/2.5/",
 }
 
 export default api
